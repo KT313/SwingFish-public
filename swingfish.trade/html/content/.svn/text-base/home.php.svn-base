40b273fdaf528d9036414c430643de8dc2998f9d
Mario Hennenberger <mario@hennenberger.de>
2017-04-11 09:42:19 -0400

    init html folder from sg

---
+++ swingfish.trade/html/content/.svn/text-base/home.php.svn-base
@@ -0,0 +1,7 @@
+	<div class="hero-unit">
+		<h1>Welcome.</h1>
+		<p>
+			SwingFish is a new Traders community .. designed to souuort each other, minus the trolls.
+		</p>
+		<hr />
+	</div>
---
+++ swingfish.trade/html/inc/form-newsletter.php
@@ -0,0 +1,13 @@
+<?
+
+/*
+expects: subscribe:test@tester.com
+
+returns:
+{"errors":"Please enter a valid email address."}
+
+or:
+{"status":1,"errors":""}
+
+*/
+?>{"errors":"Sorry, somethings went wrong."}
